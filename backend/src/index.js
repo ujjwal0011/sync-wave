e86a8ac48962420db54a636fd1b2764f5ab5d898
ujjwal bhaedwaj <ujjwalbhardwaj.2002>
2025-02-28 01:45:08 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sync-wave-1.onrender.com",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
